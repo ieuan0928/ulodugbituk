ec14be47b6162a95fb29da640afe59a7f46a043f
ieuan0928 <ieuan0928@gmail.com>
2016-08-13 14:54:25 +0800

    test

--- worm_scheme/js/gridBehavior.js
+++ worm_scheme/js/gridBehavior.js
@@ -6,11 +6,13 @@
         }, options);
 
         return this.each(function() {
-            $(this).resize(function() {
+            $(this).on("resize", function() {
                 alert("resize bullshit");
             });
             $(this).click(function() {
                 alert("bullshit");
+                alert($(this).attr("id"))
+                $(this).css("height", "200px");
             })
         })
     }
